d1ff08bb545f1818b657dc80f69b2ee9cb448561
Adam Walden <adamwalden121@gmail.com>
2026-05-13 22:15:01 -0500

    Wed May 13 22:15:01 CDT 2026 Testing Bash scripting/commands)

--- my_first_script.sh
+++ my_first_script.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-echo "=============================="
-echo " Server Monitor - $(date)"
-echo "=============================="
-echo ""
-
-echo "Running server health check..."
-python3 ~/IdeaProjects/devops_journey/day3.py
-
-echo ""
-echo "=============================="
-echo "Auto committing to GitHub..."
-cd ~/IdeaProjects/devops_journey
+NAME="Adam"
+echo "Welcome, $NAME"
+echo "Today's date is $(date)"
+HOUR=$(date +%H)
+if [ $HOUR -lt 12 ]; then
+    echo "Good Morning!"
+elif [ $HOUR -lt 17 ]; then
+    echo "Good Afternoon!"
+else
+    echo "Good Evening!"
+fi
+python3 ~/IdeaProjects/devops_journey/day3.python
 git add .
-git commit -m "Auto commit - $(date)"
+git commit -m "$(date) Testing Bash scripting/commands)"
 git push
-
-echo ""
-echo "=============================="
-echo "Done!"
+echo "All Done!"
