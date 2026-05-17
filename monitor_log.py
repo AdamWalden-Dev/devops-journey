c4f5ad687a3dd0741fcda954065db22294f8d15a
Adam Walden <adamwalden121@gmail.com>
2026-05-16 21:37:28 -0500

    Add CPU Monitoring

--- monitor_log.py
+++ monitor_log.py
@@ -15,18 +15,24 @@ def read_file():
                 disk = int(data[2])
                 memory = int(data[3])
                 location = data[4]
+                cpu = int(data[5])
                 if disk >= 999:
                     disk_status = "Too High"
                     warning_count += 1
                 else:
-                    disk_status = "Disk is fine"
+                    disk_status = "Disk is Fine"
                 if memory >= 999:
                     memory_status = "Too High"
                     warning_count += 1
                 else:
-                    memory_status = "Memory is fine"
-                print(f"{timestamp} Name:{name} - Status:{status} - Disk: {disk}|{disk_status} - Memory: {memory}|{memory_status} - Location: {location}")
-                monitor_log(name,status,disk,disk_status,memory,memory_status,location)
+                    memory_status = "Memory is Fine"
+                if cpu >= 60:
+                    cpu_status = "Too High"
+                    warning_count += 1
+                else:
+                    cpu_status = "CPU is Fine"
+                print(f"{timestamp} Name:{name} - Status:{status} - Disk: {disk}|{disk_status} - Memory: {memory}|{memory_status}- Location: {location} CPU: {cpu}|{cpu_status} ")
+                monitor_log(name,status,disk,disk_status,memory,memory_status,location,cpu,cpu_status)
             if warning_count > 0:
                 print("TOO MANY WARNINGS")
                 sys.exit(1)
@@ -37,10 +43,10 @@ def read_file():
         print(f"UNKNOWN!! - {e}")
 
 
-def monitor_log(name,status,disk,disk_status,memory,memory_status,location):
+def monitor_log(name,status,disk,disk_status,memory,memory_status,location,cpu,cpu_status):
     try:
         with open("monitor_log.txt", "a") as f:
-            f.write(f"{timestamp} Name:{name} - Status:{status} - Disk: {disk}|{disk_status} - Memory: {memory}|{memory_status} - Location: {location}\n")
+            f.write(f"{timestamp} Name:{name} - Status:{status} - Disk: {disk}|{disk_status} - Memory: {memory}|{memory_status}- Location: {location} {cpu}|{cpu_status}\n")
     except Exception as e:
         print(f"Could not Complete. {e}")
 
