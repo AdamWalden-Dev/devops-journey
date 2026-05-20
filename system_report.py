4651c27ba7910ae7ace4c4fcc021f4433b31cf20
Adam Walden <adamwalden121@gmail.com>
2026-05-19 20:06:26 -0500

    fix report formatting and add sys.exit codes

--- system_report.py
+++ system_report.py
@@ -17,13 +17,14 @@ def get_system_info():
         "memory": memory_percent,
         "disk": disk_percent
         }
+    sys.exit(0)
  
  
 
 def system_report(timestamp, systeminfo):
     try:
         with open(f"logs/report_{timestamp}.txt", "w") as f:
-            f.write(f"{timestamp}\n CPU: {systeminfo['cpu']}\n Memory: {systeminfo['memory']}\n Disk: {systeminfo['disk']}")
+            f.write(f"{timestamp}\n CPU: {systeminfo['cpu']}%\n Memory: {systeminfo['memory']}%\n Disk: {systeminfo['disk']}")
     except Exception as e:
         print(f"Unknown ERROR: {e}")
 
